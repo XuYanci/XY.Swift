acd87e9e3901be0ed982f9721ca291587aaf9e5f
XuYanci <tdw@tdwdeiMac.local>
2017-06-30 17:09:27 +0800

    * commit some

--- XYSwiftExercisePro/XYSwiftExercisePro/ViewController.swift
+++ XYSwiftExercisePro/XYSwiftExercisePro/ViewController.swift
@@ -8,14 +8,24 @@
 
 import UIKit
 class ViewController: UIViewController {
-
+    
     var basic:Basic = Basic()
     var basicOperator:BasicOperator = BasicOperator()
+    var character:Characters = Characters()
+    var arrays:Arrays = Arrays()
+    var controls:Controls = Controls()
+    var funcs:Funcs = Funcs()
+    var closures:Closures = Closures()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         basic.main()
         basicOperator.main()
+        character.main()
+        arrays.main()
+        controls.main()
+        closures.main()
     }
     
 
@@ -24,6 +34,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
