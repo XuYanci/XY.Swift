8a01f7dad40a3664eb11a7d6439004aca24c5072
xulijun <xulijun@sugarapp.cn>
2020-06-03 18:26:14 +0800

    commit some

--- swiftbysundell.playground/Contents.swift
+++ swiftbysundell.playground/Contents.swift
@@ -50,7 +50,7 @@ class A1 {
 class B1 :A1 {
     func b() {
         j = 2;
-//        i = 1; /// 'i' is inaccessible due to 'private' protection level
+        //        i = 1; /// 'i' is inaccessible due to 'private' protection level
     }
 }
 
@@ -60,7 +60,7 @@ func hashtags(in string: String) -> [String] {
     let words = string.components(
         separatedBy: .whitespacesAndNewlines
     )
-
+    
     let tags = words.filter { $0.starts(with: "#") }
     
     // Using 'map' we can convert a sequence of values into
@@ -115,7 +115,7 @@ class Triangle: Shape {
         self.hypotenuse = hypotenuse
         super.init(sides: 3, named: "Triangle")
     }
-
+    
     override func printShapeDescription() {
         super.printShapeDescription()
         print("Hypotenuse :\(self.hypotenuse)")
@@ -144,20 +144,90 @@ type(of: x).myFunc() //myOtherClass
 x = MyClass()
 type(of: x).myFunc() //myClass
 
-var string = "12121"
-var keyword = "12"
- 
-
-var string1 = string
-
-while((string1 as NSString).range(of: keyword).location != NSNotFound) {
-    let _r1 = string1.range(of: keyword)
-    string1.removeSubrange(_r1!)
-    print(_r1)
+protocol Tagged: Hashable {
+    var tags: [String] { get }
 }
 
+struct TaggedCollection<Element: Tagged> {
+    private var elements = [String : Set<Element>]()
+    
+    mutating func add(_ element: Element) {
+        for tag in element.tags {
+            elements[tag, default: []].insert(element)
+        }
+    }
+    
+    mutating func remove(_ element: Element) {
+        for tag in element.tags {
+            elements[tag]?.remove(element)
+        }
+    }
+    
+    func elements(taggedWith tag: String) -> Set<Element> {
+        elements[tag] ?? []
+    }
+}
 
- 
-var users: [Int: Bool] = [:]
-users[21069] = true
+ func getCombineItemsAndInsertIndexPaths(originList:[Int],newList:[Int],isDuplicateRemove:Bool = false) -> (combineList:[Int],insertIndexPaths:[IndexPath]) {
+     var insertIndexPaths = [IndexPath]()
+     
+     /// Do not remove duplicate
+     guard isDuplicateRemove else {
+         insertIndexPaths = (originList.count..<originList.count + newList.count).map({IndexPath(row: $0, section: 0)})
+         let combineItems = originList + newList
+         return (combineItems,insertIndexPaths)
+     }
+     
+     /// Remove duplicate items
+     let oldListCount = originList.count
+     var combineItems = originList + newList
+     combineItems = combineItems.unique
+     
+     let insertListCount = combineItems.count - oldListCount
+     if insertListCount > 0 {
+         insertIndexPaths = (0..<insertListCount).map({IndexPath(row: oldListCount + $0, section: 0)})
+     }
+     return (combineItems,insertIndexPaths)
+     
+ }
+
+/// 去重拓展
+extension Array where Element: Hashable {
+    /// 去重结果
+    var unique: [Element] {
+        var uniq = Set<Element>()
+        uniq.reserveCapacity(self.count)
+        return self.filter {
+            return uniq.insert($0).inserted
+        }
+    }
+}
+
+var originList = [1,2,2,2,5,6]
+var newList = [30,3,6,22,2,2]
+
+let combine = getCombineItemsAndInsertIndexPaths(originList: originList, newList: newList,isDuplicateRemove: true)
+print(combine)
+
+
+public struct Tag: Hashable, Codable {
+    public var string: String
+
+    public init(_ string: String) {
+        self.string = string
+    }
+}
+
+extension Tag: ExpressibleByStringLiteral {
+    public init(stringLiteral value: String) {
+        string = value
+    }
+}
+
+public extension Tag {
+    static var newRelease: Self { #function }
+    static var onSale: Self { #function }
+    static var promoted: Self { #function }
+}
+
 
